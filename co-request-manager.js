439ba1115439c1b61a6b8cde9c533d1e41979868
Carl-Ola Boketoft <calleboketoft@gmail.com>
2016-07-28 15:07:03 +0200

    first version

---
+++ co-request-manager.js
@@ -0,0 +1,6 @@
+"use strict";
+var co_request_manager_provider_1 = require('./client-src/request-manager/co-request-manager.provider');
+exports.provideCoRequestManager = co_request_manager_provider_1.provideCoRequestManager;
+var co_request_manager_component_1 = require('./client-src/request-manager/co-request-manager.component');
+exports.CoRequestManagerComponent = co_request_manager_component_1.CoRequestManagerComponent;
+//# sourceMappingURL=co-request-manager.js.map
---
+++ co-request-manager.ts
@@ -0,0 +1,2 @@
+export {provideCoRequestManager} from './client-src/request-manager/co-request-manager.provider'
+export {CoRequestManagerComponent} from './client-src/request-manager/co-request-manager.component'
